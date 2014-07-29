532b35ffaaced65d88404ab7bd0a33a10d433729
Bene <benestar.wikimedia@googlemail.com>
2014-07-29 23:59:11 +0200

    Fix usage of global variable

--- WikidataOrg.php
+++ WikidataOrg.php
@@ -45,7 +45,7 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	include_once( __DIR__ . '/vendor/autoload.php' );
 }
 
-$wgExtensionFunctions[] = function() {
+$GLOBALS['wgExtensionFunctions'][] = function() {
 	global $wgExtensionCredits, $wgMessagesDirs, $wgHooks, $wgResourceModules;
 
 	if ( !defined( 'WB_VERSION' ) ) {
