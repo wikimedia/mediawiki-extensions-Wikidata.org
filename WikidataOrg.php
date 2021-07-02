842e4a41466576c0dcba857a4f3c7df34da4bfdd
James D. Forrester <jforrester@wikimedia.org>
2021-07-02 15:37:00 +0100

    Follow-up ded7b75de0: Uncomment the wfWarn for deprecated entry
    It's not clear why this wasn't added from the start.
    
    Bug: T140851
    Change-Id: I9684d881b01bceb9cce26fc171ff1d9598bd226e

--- WikidataOrg.php
+++ WikidataOrg.php
@@ -4,10 +4,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'Wikidata.org', __DIR__ . '/extension.json' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['Wikidata.org'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for Wikidata.org extension. Please use wfLoadExtension ' .
 		'instead, see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return true;
 } else {
 	die( 'This version of the Wikidata.org extension requires MediaWiki 1.29+' );
